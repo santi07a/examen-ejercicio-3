469ca4a66ade99f0e71de25738326f34ad98998d
Santi Arnau <santi07a@gmail.com>
2021-03-23 10:42:32 +0100

    quitado maqueta prueba

--- src/Componentes/Serie.js
+++ src/Componentes/Serie.js
@@ -2,8 +2,6 @@ const Serie = () => {
   return (
     <>
       <li className="serie"><img className="poster" src="https://m.media-amazon.com/images/M/MV5BMTQzODk5N2UtYjE4ZC00YWM1LWJkMGYtYmY2NjNjMjIzZDRmXkEyXkFqcGdeQXVyNTE1NjY5Mg@@._V1_SX300.jpg" alt="Six feet under" />Six feet under (HBO, año 2001)</li>
-      <li className="serie"><img className="poster" src="img/tv.jpg" alt="Six feet under" />Casi feliz (Netflix, año 2020)</li>
-      <li className="serie"><img className="poster" src="https://m.media-amazon.com/images/M/MV5BYjg1ZmQ0ZDktMjNhZS00MDJjLTllZTItMmIyYTBkYjM2MzQzXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_SX300.jpg" alt="Six feet under" />Atypical (Netflix, año 2017)</li>
     </>
   );
 };
